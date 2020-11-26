e7bf21858b2caf86d3de4ab002541d6d07e9370e
Thibault Sordet <sordetthibault@gmail.com>
2020-11-26 11:29:35 +0100

    update the link

--- Sign_in.js
+++ Sign_in.js
@@ -1,4 +1,4 @@
-const button = document.querySelector("#submit_button")
-button.addEventListener("click", (event) => {
-  window.open('https://b-payments.herokuapp.com/', '_blank'); // this is the where we will need to call the api from our app
+const signup = document.querySelector("a")
+signup.addEventListener("click", (event) => {
+  window.open('https://b-payments.herokuapp.com/users/sign_up', '_blank'); // this is the where we will need to call the api from our app
 });
